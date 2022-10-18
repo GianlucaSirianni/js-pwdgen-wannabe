4386933f298ee2206e87303aba4de1811ede0aae
Gianluca Sirianni <gianluca.sirianni7@gmail.com>
2022-10-18 16:02:39 +0200

    esercizio completato

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,14 @@
-let divHtml = document.getElementById("ageDiv");
-divHtml.innerHTML = "<p> Hello World!</p>"
+let nameUser = prompt("What's your name?");
+console.log(nameUser);
+
+let surnameUser = prompt("What's your surname?");
+console.log(surnameUser);
+
+let favoriteCol = prompt("What's your favorite color?");
+console.log(favoriteCol);
+
+let favoriteNum = prompt("What's your favorite number?");
+console.log(favoriteNum);
+
+
+document.getElementById("pswDiv").innerHTML = "Your incredibly unsafe password is: " + nameUser + surnameUser + favoriteCol + favoriteNum;
